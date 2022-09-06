0bcbbac6618c8a5023568061e091a0a9d248a06a
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-09-06 17:57:58 +0900

    export の修正
    components/Typography から Heading を import できるように index.ts を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Heading } from 'components/Typography/Heading';
+import { Heading } from 'components/Typography';
 
 const Home: NextPage = () => {
   return (
