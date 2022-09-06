32f4536b61ca2faff7cc2f39a1f167847b26cae8
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-09-06 23:36:58 +0900

    storybook に画像を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,14 +4,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
+    'storybook-addon-next',
   ],
   framework: '@storybook/react',
   core: {
