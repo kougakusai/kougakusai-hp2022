4c25148300d6348c56b37808f95a4b152cf2182e
minusVoid <68424763+minusvoid@users.noreply.github.com>
2022-09-09 18:29:24 +0900

    `HomeButton`実装

--- src/components/Button/HomeButton/HomeButton.tsx
+++ src/components/Button/HomeButton/HomeButton.tsx
@@ -1,1 +1,32 @@
-export const HomeButton = () => <>HomeButton</>;
+import clsx from 'clsx';
+import Image, { ImageProps } from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ComponentPropsWithoutRef, MouseEvent, MouseEventHandler } from 'react';
+
+export const HomeButton = ({
+  onClick,
+  className,
+  title,
+  ...restProps
+}: ComponentPropsWithoutRef<'button'>) => {
+  const router = useRouter();
+  return (
+    <button
+      onClick={(e) => {
+        router.push('/');
+        onClick && onClick(e);
+      }}
+      className={clsx('h-[68px] w-[68px]', className)}
+      title={title || 'ホーム'}
+      {...restProps}
+    >
+      <Image
+        src="/kougakusai_logo.png"
+        alt="こうがく祭ロゴ"
+        width={68}
+        height={68}
+      />
+    </button>
+  );
+};
