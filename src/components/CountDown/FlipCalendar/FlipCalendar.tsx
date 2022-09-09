3e4d510bdaee26917cc8dbb454176f95ba6d4314
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-09-09 22:24:45 +0900

    FlipCalendar実装

--- src/components/CountDown/FlipCalendar/FlipCalendar.tsx
+++ src/components/CountDown/FlipCalendar/FlipCalendar.tsx
@@ -1,1 +1,16 @@
-export const FlipCalendar = () => <>FlipCalendar</>;
+import { ComponentPropsWithoutRef } from 'react';
+import Image from 'next/image';
+import clsx from 'clsx';
+
+export const FlipCalendar = ({
+  className,
+  children,
+  ...rest
+}: ComponentPropsWithoutRef<'div'>) => (
+  <div className={clsx('relative h-[140px] w-[70px]', className)} {...rest}>
+    <Image src="/flip_clock.png" alt="カウントダウン" width={70} height={140} />
+    <p className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-[Roboto] text-[48px] font-bold leading-[56px]">
+      {children}
+    </p>
+  </div>
+);
