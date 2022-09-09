462417f148174dc154242bd4055a486746e4ec40
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-09-09 21:46:30 +0900

    useCountDown実装

--- src/hooks/useCountDown/useCountDown.ts
+++ src/hooks/useCountDown/useCountDown.ts
@@ -1,1 +1,16 @@
-export const useCountDown = () => {};
+/**
+ * useCountDownはシステムの現在日時から日本時間2022年11月5日までの日数を返します
+ * 返り値は必要に応じて0でpaddingされ常に2桁になります
+ */
+export const useCountDown = () => {
+  const msPerDay = 24 * 60 * 60 * 1000;
+
+  const now = Date.now();
+  const target = Date.UTC(2022, 10, 4, 15, 0, 0);
+  const daysLeft = Math.floor((target - now) / msPerDay);
+
+  const daysLeftStr =
+    daysLeft < 0 ? '00' : daysLeft.toString().padStart(2, '0');
+
+  return [daysLeftStr.slice(0, 1), daysLeftStr.slice(1, 2)] as const;
+};
