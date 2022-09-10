11cbccb33d240d6848c3977ac3428c9d61450d90
minusVoid <68424763+minusvoid@users.noreply.github.com>
2022-09-10 16:53:05 +0900

    Desktop表示対応

--- src/components/Button/HomeButton/HomeButton.tsx
+++ src/components/Button/HomeButton/HomeButton.tsx
@@ -1,29 +1,56 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, useEffect, useReducer } from 'react';
+
+const smSize = 68;
+const lgSize = 153;
+const breakPoint = 1000;
 
 export const HomeButton = ({
   onClick,
   className,
+  title,
   ...restProps
-}: Omit<ComponentPropsWithoutRef<'button'>, 'title'>) => {
+}: ComponentPropsWithoutRef<'button'>) => {
   const router = useRouter();
+
+  const useMobileView = () => {
+    const [matches, setMatches] = useReducer(
+      () =>
+        window
+          ? window.matchMedia(`(max-width:${breakPoint}px)`).matches
+          : true,
+      true
+    );
+    useEffect(() => {
+      const matchMedia = window.matchMedia(`(max-width:${breakPoint}px)`);
+      matchMedia.addEventListener('change', setMatches);
+      setMatches();
+
+      return () => matchMedia.removeEventListener('change', setMatches);
+    }, []);
+
+    return matches;
+  };
+
+  const isMobile = useMobileView();
+
   return (
     <button
       onClick={(e) => {
         router.push('/');
         onClick && onClick(e);
       }}
-      className={clsx('h-[68px] w-[68px]', className)}
-      title="ホーム"
+      className={clsx(`h-[68px] w-[68px] lg:h-[153px] lg:w-[153px]`, className)}
+      title={title || 'ホーム'}
       {...restProps}
     >
       <Image
-        src="/kougakusai_logo.png"
+        src="/kougakusai_logo.svg"
         alt="こうがく祭ロゴ"
-        width={68}
-        height={68}
+        width={isMobile ? smSize : lgSize}
+        height={isMobile ? smSize : lgSize}
       />
     </button>
   );
