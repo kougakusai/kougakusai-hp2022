f2d9e859008e71b42cbd4c3956e0f17b17be7ee7
ayagifc <79235899+ayagifc@users.noreply.github.com>
2022-09-11 20:51:15 +0900

    影の追加

--- src/components/Button/MenuButton/MenuButton.tsx
+++ src/components/Button/MenuButton/MenuButton.tsx
@@ -8,13 +8,13 @@ export const MenuButton = ({
 }: ComponentPropsWithoutRef<'button'>) => {
   return (
     <button
-      className={'w-12 h-12 sm:w-28 sm:h-28 relative ' + className}
+      className={'w-12 h-12 sm:w-28 sm:h-28 relative drop-shadow-sm ' + className}
       {...restProps}
     >
       <span className="absolute top-0.5 left-1.5 z-10 text-center text-xs font-bold leading-3 text-gray-800 sm:top-1 sm:left-4 sm:w-20 sm:text-2xl sm:leading-7">
         MENU
       </span>
-      <div className="box-border h-12 w-12 rounded-sm border border-gray-400 p-1 sm:h-28 sm:w-28 sm:p-2">
+      <div className="box-border h-12 w-12 rounded-sm border border-gray-400 p-1 shadow-sm sm:h-28 sm:w-28 sm:p-2">
         <Image
           src={menuIcon}
           alt="menuIcon"
