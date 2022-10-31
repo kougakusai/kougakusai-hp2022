9223cf417b9bf674a7bc7e7af5593469fb9a3430
minusVoid <68424763+minusvoid@users.noreply.github.com>
2022-10-31 16:22:33 +0900

    `KitchenCarImg`：置き換え

--- src/pages/kitchen-car.tsx
+++ src/pages/kitchen-car.tsx
@@ -1,33 +1,8 @@
 import type { NextPageWithLayout } from 'pages/_app';
 import Head from 'next/head';
 import { DefaultLayout } from 'components/Layouts/DefaultLayout';
-import { ComponentPropsWithoutRef } from 'react';
-import clsx from 'clsx';
-import Image from 'next/image';
 import { kitchenCarData } from 'data/kitchen_car';
-
-const KitchenCarImg = ({
-  data,
-  className,
-  ...restProps
-}: {
-  data: { name: string; src: string; description: string | string[] };
-} & ComponentPropsWithoutRef<'section'>) => (
-  <section
-    className={clsx('w-[18.75rem] bg-black p-1', className)}
-    {...restProps}
-  >
-    <h2 className="ml-1 text-start font-[sans-serif] text-xs text-white">
-      {data.name}
-    </h2>
-    <Image src={data.src} alt={data.name} width={292} height={120} />
-    <p className="flex items-center justify-around text-center font-[sans-serif] text-[0.5rem] text-white">
-      {typeof data.description === 'string'
-        ? data.description
-        : data.description.map((text) => <span key={text}>{text}</span>)}
-    </p>
-  </section>
-);
+import { KitchenCarImg } from 'components/KitchenCar';
 
 const KitchenCar: NextPageWithLayout = () => {
   return (
