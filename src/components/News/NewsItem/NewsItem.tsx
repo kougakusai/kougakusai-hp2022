6dec750ea61b03c6fa8571e441529e9a7bd4490e
minusVoid <68424763+minusvoid@users.noreply.github.com>
2022-11-02 15:34:47 +0900

    `Topお知らせ`：リンク化

--- src/components/News/NewsItem/NewsItem.tsx
+++ src/components/News/NewsItem/NewsItem.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef } from 'react';
 import { Paragraph, Small } from 'components/Typography';
 import type { newsDataItemType } from 'data';
+import Link, { LinkProps } from 'next/link';
 
 export const NewsItem = ({
   date,
@@ -8,13 +9,15 @@ export const NewsItem = ({
   title,
   detail,
   ...restProps
-}: ComponentPropsWithoutRef<'div'> & newsDataItemType) => (
-  <div {...restProps}>
-    <div className="flex gap-[8px]">
-      <Small>{date}</Small>
-      <Small>|</Small>
-      <Small>{category}</Small>
-    </div>
-    <Paragraph className="mt-[8px]">{title}</Paragraph>
-  </div>
+}: LinkProps & newsDataItemType) => (
+  <Link {...restProps}>
+    <a>
+      <div className="flex gap-[8px]">
+        <Small>{date}</Small>
+        <Small>|</Small>
+        <Small>{category}</Small>
+      </div>
+      <Paragraph className="mt-[8px]">{title}</Paragraph>
+    </a>
+  </Link>
 );
